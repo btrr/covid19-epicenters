bb70baa46a8f2210a4ecf1670068409c7234f2ca
Octavia Keating <ok@Octavias-MacBook-Air.local>
2020-03-29 01:20:52 -0400

    feat: Add four graphs for NYC

---
+++ nyc/nyc-deaths-tot.py
@@ -0,0 +1,37 @@
+# line graph of total deaths in NYC
+
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
+
+dates = ['3/12/2020', '3/13/2020', '3/14/2020', '3/15/2020', '3/16/2020', '3/17/2020', '3/18/2020', '3/19/2020', '3/20/2020', '3/21/2020', '3/22/2020', '3/23/2020', '3/24/2020', '3/25/2020', '3/26/2020', '3/27/2020']
+
+# format dates
+x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+ax = plt.gca()
+formatter = mdates.DateFormatter("%m/%d")
+ax.xaxis.set_major_formatter(formatter)
+
+# create x-axis
+# major tick = every 3 days
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU), interval=4))
+# minor tick = daily
+ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU)))
+
+# format y-axis
+ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
+
+# new deaths by day
+total_deaths = [0, 1, 2, 5, 7, 10, 11, 22, 43, 60, 99, 125, 192, 280, 365, 450]
+
+# text labels
+plt.title('Covid-19 Deaths in NYC')
+plt.xlabel('Date')
+plt.ylabel('Number of Deaths')
+
+# create the graph
+plt.plot(x_values, total_deaths)
+
+plt.show()
