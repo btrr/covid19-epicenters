d69c8d8c4dabff89de4f0b5fcdc049631cffcaff
Octavia Keating <ok@Octavias-MacBook-Air.local>
2020-03-29 01:21:46 -0400

    feat: Add four graphs for Bergen County

---
+++ bergen-county/bergen-deaths-new.py
@@ -0,0 +1,37 @@
+# line graph of new deaths in Bergen County, NJ
+
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
+
+dates = ['3/9/2020', '3/10/2020', '3/11/2020', '3/12/2020', '3/13/2020', '3/14/2020', '3/15/2020', '3/16/2020', '3/17/2020', '3/18/2020', '3/19/2020', '3/20/2020', '3/21/2020', '3/22/2020', '3/23/2020', '3/24/2020', '3/25/2020', '3/26/2020', '3/27/2020']
+
+# format dates
+x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+ax = plt.gca()
+formatter = mdates.DateFormatter("%m/%d")
+ax.xaxis.set_major_formatter(formatter)
+
+# create x-axis
+# major tick = every 3 days
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU), interval=4))
+# minor tick = daily
+ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU)))
+
+# format y-axis
+ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
+
+# new deaths by day
+new_deaths = [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 2, 1, 0, 4, 4, 3, 2]
+
+# text labels
+plt.title('Covid-19 New Deaths Daily in Bergen County, NJ')
+plt.xlabel('Date')
+plt.ylabel('Number of Deaths')
+
+# create the graph
+plt.plot(x_values, new_deaths)
+
+plt.show()
---
+++ bergen-county/bergen-new-cases.py
@@ -0,0 +1,37 @@
+# line graph of new cases in Bergen County, NJ
+
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
+
+dates = ['3/3/2020', '3/4/2020', '3/5/2020', '3/6/2020', '3/7/2020', '3/8/2020', '3/9/2020', '3/10/2020', '3/11/2020', '3/12/2020', '3/13/2020', '3/14/2020', '3/15/2020', '3/16/2020', '3/17/2020', '3/18/2020', '3/19/2020', '3/20/2020', '3/21/2020', '3/22/2020', '3/23/2020', '3/24/2020', '3/25/2020', '3/26/2020', '3/27/2020']
+
+# format dates
+x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+ax = plt.gca()
+formatter = mdates.DateFormatter("%m/%d")
+ax.xaxis.set_major_formatter(formatter)
+
+# create x-axis
+# major tick = every 3 days
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU), interval=4))
+# minor tick = daily
+ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, SA, SU)))
+
+# format y-axis
+ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
+
+# new cases by day
+new_cases = [0, 1, 1, 1, 0, 1, 1, 2, 4, 2, 2, 10, 7, 10, 44, 27, 81, 55, 114, 94, 152, 76, 172, 349, 299]
+
+# text labels
+plt.title('Covid-19 New Cases Each Day in Bergen County, NJ')
+plt.xlabel('Date')
+plt.ylabel('Number of New Cases')
+
+# create the graph
+plt.plot(x_values, new_cases)
+
+plt.show()
