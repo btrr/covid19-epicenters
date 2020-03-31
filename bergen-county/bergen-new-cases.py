a1345f2bcd72092b1adfbb5eeb8aad9d6b39db20
Octavia Keating <ok@Octavias-MacBook-Air.local>
2020-03-31 10:32:23 -0400

    feat: Add significant containment measures to NYC and Bergen charts

--- bergen-county/bergen-new-cases.py
+++ bergen-county/bergen-new-cases.py
@@ -1,6 +1,6 @@
 # line graph of new cases in Bergen County, NJ
 
-import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
@@ -9,7 +9,7 @@ from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
 dates = ['3/3/2020', '3/4/2020', '3/5/2020', '3/6/2020', '3/7/2020', '3/8/2020', '3/9/2020', '3/10/2020', '3/11/2020', '3/12/2020', '3/13/2020', '3/14/2020', '3/15/2020', '3/16/2020', '3/17/2020', '3/18/2020', '3/19/2020', '3/20/2020', '3/21/2020', '3/22/2020', '3/23/2020', '3/24/2020', '3/25/2020', '3/26/2020', '3/27/2020', '3/28/2020', '3/29/2020', '3/30/2020']
 
 # format dates
-x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+x_values = [dt.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
 ax = plt.gca()
 formatter = mdates.DateFormatter("%m/%d")
 ax.xaxis.set_major_formatter(formatter)
@@ -23,6 +23,11 @@ ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, 
 # format y-axis
 ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
 
+# schools closed
+plt.axvline(dt.datetime(2020, 3, 18), linestyle='--', color='orange', linewidth=2, label='schools')
+# stay-at-home
+plt.axvline(dt.datetime(2020, 3, 21), color='black', linewidth=2, label='stay at home')
+
 # new cases by day
 new_cases = [0, 1, 1, 1, 0, 1, 1, 2, 4, 2, 2, 10, 7, 10, 44, 27, 81, 55, 114, 94, 152, 76, 172, 349, 299, 327, 337, 313]
 
@@ -30,6 +35,7 @@ new_cases = [0, 1, 1, 1, 0, 1, 1, 2, 4, 2, 2, 10, 7, 10, 44, 27, 81, 55, 114, 94
 plt.title('Covid-19 in Bergen County, NJ: New Confirmed Diagnoses')
 plt.xlabel('Date')
 plt.ylabel('Number of New Cases')
+plt.legend(['Schools Closure', 'Non-Essential Businesses Closure', 'Statewide Stay-at-Home Order'], loc='upper left')
 
 # create the graph
 plt.plot(x_values, new_cases, color='#730af2', linewidth=2)
--- nyc/nyc-new-cases.py
+++ nyc/nyc-new-cases.py
@@ -1,6 +1,6 @@
 # line graph of new cases in NYC
 
-import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
@@ -9,7 +9,7 @@ from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
 dates = ['2/29/2020', '3/1/2020', '3/2/2020', '3/3/2020', '3/4/2020', '3/5/2020', '3/6/2020', '3/7/2020', '3/8/2020', '3/9/2020', '3/10/2020', '3/11/2020', '3/12/2020', '3/13/2020', '3/14/2020', '3/15/2020', '3/16/2020', '3/17/2020', '3/18/2020', '3/19/2020', '3/20/2020', '3/21/2020', '3/22/2020', '3/23/2020', '3/24/2020', '3/25/2020', '3/26/2020', '3/27/2020', '3/28/2020', '3/29/2020', '3/30/2020']
 
 # format dates
-x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+x_values = [dt.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
 ax = plt.gca()
 formatter = mdates.DateFormatter("%m/%d")
 ax.xaxis.set_major_formatter(formatter)
@@ -23,6 +23,13 @@ ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, 
 # format y-axis
 ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
 
+# schools closed
+plt.axvline(dt.datetime(2020, 3, 18), linestyle='--', color='orange', linewidth=2, label='schools')
+# non-essential businesses closed
+plt.axvline(dt.datetime(2020, 3, 20), linestyle='--', color='red', linewidth=2, label='nonessential')
+# stay-at-home
+plt.axvline(dt.datetime(2020, 3, 22), color='black', linewidth=2, label='stay at home')
+
 # new cases by day
 new_cases = [0, 1, 0, 0, 0, 3, 0, 7, 9, 7, 5, 23, 47, 59, 115, 60, 485, 109, 1086, 1606, 2068, 2432, 2649, 2355, 2478, 4414, 3101, 3585, 4033, 2744, 4613]
 
@@ -30,6 +37,7 @@ new_cases = [0, 1, 0, 0, 0, 3, 0, 7, 9, 7, 5, 23, 47, 59, 115, 60, 485, 109, 108
 plt.title('Covid-19 in NYC: New Cases')
 plt.xlabel('Date')
 plt.ylabel('Number of New Cases')
+plt.legend(['Schools Closure', 'Non-Essential Businesses Closure', 'Statewide Stay-at-Home Order'], loc='upper left')
 
 # create the graph
 plt.plot(x_values, new_cases, color='#730af2', linewidth=2)
