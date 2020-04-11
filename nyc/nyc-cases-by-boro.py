f44e606053f68c89796c124a92991e01fe77bcca
BTRR <btrr@github.com>
2020-04-11 13:51:50 -0400

    chore: Update data for NYC for 4/10

--- nyc/nyc-cases-by-boro.py
+++ nyc/nyc-cases-by-boro.py
@@ -4,7 +4,7 @@ import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
 
-dates = ['4/2/2020', '4/3/2020', '4/4/2020', '4/5/2020', '4/6/2020', '4/7/2020', '4/8/2020', '4/9/2020']
+dates = ['4/2/2020', '4/3/2020', '4/4/2020', '4/5/2020', '4/6/2020', '4/7/2020', '4/8/2020', '4/9/2020', '4/10/2020']
 
 # format dates
 x_values = [datetime.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
@@ -20,15 +20,15 @@ ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO, TU, WE, TH, FR, 
 # format y-axis
 ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: format(int(x), ',')))
 
-bronx = [8.55, 15.22, 9.8, 7.77, 7.4, 9.22, 9.89, 14.11]
+bronx = [8.55, 15.22, 9.8, 7.77, 7.4, 9.22, 9.89, 14.11, 9.64]
 
-brooklyn = [8.28, 15.33, 7.57, 6.26, 5.22, 9.77, 6.65, 8.47]
+brooklyn = [8.28, 15.33, 7.57, 6.26, 5.22, 9.77, 6.65, 8.47, 6.14]
 
-manhattan = [5.35, 11.14, 6.8, 5.35, 4.76, 5.81, 5.93, 5.74]
+manhattan = [5.35, 11.14, 6.8, 5.35, 4.76, 5.81, 5.93, 5.74, 6.22]
 
-queens = [10.53, 11.92, 8.22, 6.92, 5.98, 7.48, 5.62, 5.93]
+queens = [10.53, 11.92, 8.22, 6.92, 5.98, 7.48, 5.62, 5.93, 7.19]
 
-staten = [10.58, 10.45, 7.64, 8.14, 6.15, 12.31, 17.97, 23.44]
+staten = [10.58, 10.45, 7.64, 8.14, 6.15, 12.31, 17.97, 23.44, 11.58]
 
 
 # create the graph
